e95b565948b6ceac73f754cab7334e837e73a729
roichan-creator <aryshaputra258730@student.smkn1kandeman.sch.id>
2025-11-14 20:12:14 +0700

    Add files via upload

---
+++ projek akhir/projekAkhir_RoichanAryshaputra/projekAkhir_RoichanAryshaputra/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace projekAkhir_RoichanAryshaputra
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            lblJudul = new Label();
+            lblNama = new Label();
+            txtNama = new TextBox();
+            lblKategori = new Label();
+            cmbKategori = new ComboBox();
+            lblPilihMenu = new Label();
+            cmbPilihMenu = new ComboBox();
+            btnTambah = new Button();
+            lblPesanan = new Label();
+            lstPesanan = new ListBox();
+            btnHapus = new Button();
+            btnReset = new Button();
+            lblTotal = new Label();
+            btnSimpan = new Button();
+            btnRiwayat = new Button();
+            btnKurangi = new Button();
+            SuspendLayout();
+            // 
+            // lblJudul
+            // 
+            lblJudul.AutoSize = true;
+            lblJudul.Font = new Font("Bahnschrift", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblJudul.ForeColor = Color.FromArgb(255, 188, 13);
+            lblJudul.Location = new Point(58, 9);
+            lblJudul.Name = "lblJudul";
+            lblJudul.Size = new Size(458, 41);
+            lblJudul.TabIndex = 0;
+            lblJudul.Text = "Selamat Datang di McDonald'";
+            // 
+            // lblNama
+            // 
+            lblNama.Font = new Font("Arial", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblNama.ForeColor = Color.FromArgb(255, 188, 13);
+            lblNama.Location = new Point(30, 100);
+            lblNama.Name = "lblNama";
+            lblNama.Size = new Size(62, 25);
+            lblNama.TabIndex = 1;
+            lblNama.Text = "Nama";
+            // 
+            // txtNama
+            // 
+            txtNama.Location = new Point(160, 100);
+            txtNama.Name = "txtNama";
+            txtNama.Size = new Size(270, 28);
+            txtNama.TabIndex = 2;
+            // 
+            // lblKategori
+            // 
+            lblKategori.Font = new Font("Arial", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblKategori.ForeColor = Color.FromArgb(255, 188, 13);
+            lblKategori.Location = new Point(30, 145);
+            lblKategori.Name = "lblKategori";
+            lblKategori.Size = new Size(100, 25);
+            lblKategori.TabIndex = 3;
+            lblKategori.Text = "Kategori";
+            // 
+            // cmbKategori
+            // 
+            cmbKategori.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbKategori.Font = new Font("Arial", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            cmbKategori.FormattingEnabled = true;
+            cmbKategori.Location = new Point(160, 145);
+            cmbKategori.Name = "cmbKategori";
+            cmbKategori.Size = new Size(270, 27);
+            cmbKategori.TabIndex = 4;
+            cmbKategori.SelectedIndexChanged += cmbKategori_SelectedIndexChanged;
+            // 
+            // lblPilihMenu
+            // 
+            lblPilihMenu.Font = new Font("Arial", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPilihMenu.ForeColor = Color.FromArgb(255, 188, 13);
+            lblPilihMenu.Location = new Point(30, 190);
+            lblPilihMenu.Name = "lblPilihMenu";
+            lblPilihMenu.Size = new Size(100, 25);
+            lblPilihMenu.TabIndex = 5;
+            lblPilihMenu.Text = "Pilih Menu";
+            // 
+            // cmbPilihMenu
+            // 
+            cmbPilihMenu.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbPilihMenu.Font = new Font("Arial", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            cmbPilihMenu.FormattingEnabled = true;
+            cmbPilihMenu.Location = new Point(30, 215);
+            cmbPilihMenu.Name = "cmbPilihMenu";
+            cmbPilihMenu.Size = new Size(440, 27);
+            cmbPilihMenu.TabIndex = 6;
+            cmbPilihMenu.SelectedIndexChanged += cmbPilihMenu_SelectedIndexChanged;
+            // 
+            // btnTambah
+            // 
+            btnTambah.BackColor = Color.FromArgb(218, 41, 28);
+            btnTambah.Cursor = Cursors.Hand;
+            btnTambah.FlatStyle = FlatStyle.Flat;
+            btnTambah.Location = new Point(476, 209);
+            btnTambah.Name = "btnTambah";
+            btnTambah.Size = new Size(102, 35);
+            btnTambah.TabIndex = 7;
+            btnTambah.Text = "Tambah";
+            btnTambah.UseVisualStyleBackColor = false;
+            btnTambah.Click += btnTambah_Click;
+            // 
+            // lblPesanan
+            // 
+            lblPesanan.AutoSize = true;
+            lblPesanan.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPesanan.ForeColor = Color.FromArgb(41, 41, 41);
+            lblPesanan.Location = new Point(30, 265);
+            lblPesanan.Name = "lblPesanan";
+            lblPesanan.Size = new Size(173, 24);
+            lblPesanan.TabIndex = 8;
+            lblPesanan.Text = "\U0001f6d2Pesanan Kamu";
+            // 
+            // lstPesanan
+            // 
+            lstPesanan.BackColor = Color.White;
+            lstPesanan.BorderStyle = BorderStyle.FixedSingle;
+            lstPesanan.Font = new Font("Arial", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lstPesanan.FormattingEnabled = true;
+            lstPesanan.ItemHeight = 19;
+            lstPesanan.Location = new Point(30, 295);
+            lstPesanan.Name = "lstPesanan";
+            lstPesanan.Size = new Size(580, 173);
+            lstPesanan.TabIndex = 9;
+            lstPesanan.SelectedIndexChanged += lstPesanan_SelectedIndexChanged;
+            // 
+            // btnHapus
+            // 
+            btnHapus.BackColor = Color.FromArgb(239, 68, 68);
+            btnHapus.Cursor = Cursors.Hand;
+            btnHapus.FlatStyle = FlatStyle.Flat;
+            btnHapus.Font = new Font("Arial", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnHapus.Location = new Point(30, 485);
+            btnHapus.Name = "btnHapus";
+            btnHapus.Size = new Size(140, 38);
+            btnHapus.TabIndex = 10;
+            btnHapus.Text = "× Hapus";
+            btnHapus.UseVisualStyleBackColor = false;
+            btnHapus.Click += btnHapus_Click;
+            // 
+            // btnReset
+            // 
+            btnReset.BackColor = Color.FromArgb(107, 114, 128);
+            btnReset.Cursor = Cursors.Hand;
+            btnReset.FlatStyle = FlatStyle.Flat;
+            btnReset.Font = new Font("Arial", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnReset.Location = new Point(184, 485);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(140, 38);
+            btnReset.TabIndex = 11;
+            btnReset.Text = "Reset";
+            btnReset.UseVisualStyleBackColor = false;
+            btnReset.Click += btnReset_Click;
+            // 
+            // lblTotal
+            // 
+            lblTotal.Location = new Point(30, 538);
+            lblTotal.Name = "lblTotal";
+            lblTotal.Size = new Size(580, 50);
+            lblTotal.TabIndex = 12;
+            lblTotal.Text = "TOTAL: Rp 0";
+            lblTotal.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // btnSimpan
+            // 
+            btnSimpan.BackColor = Color.FromArgb(34, 197, 94);
+            btnSimpan.Cursor = Cursors.Hand;
+            btnSimpan.FlatStyle = FlatStyle.Flat;
+            btnSimpan.Location = new Point(30, 594);
+            btnSimpan.Name = "btnSimpan";
+            btnSimpan.Size = new Size(280, 45);
+            btnSimpan.TabIndex = 13;
+            btnSimpan.Text = "💳 CHECKOUT";
+            btnSimpan.UseVisualStyleBackColor = false;
+            btnSimpan.Click += btnSimpan_Click;
+            // 
+            // btnRiwayat
+            // 
+            btnRiwayat.BackColor = Color.FromArgb(255, 188, 13);
+            btnRiwayat.Cursor = Cursors.Hand;
+            btnRiwayat.FlatStyle = FlatStyle.Flat;
+            btnRiwayat.ForeColor = Color.FromArgb(41, 41, 41);
+            btnRiwayat.Location = new Point(340, 591);
+            btnRiwayat.Name = "btnRiwayat";
+            btnRiwayat.Size = new Size(280, 45);
+            btnRiwayat.TabIndex = 14;
+            btnRiwayat.Text = "📋 Riwayat";
+            btnRiwayat.UseVisualStyleBackColor = false;
+            btnRiwayat.Click += btnRiwayat_Click;
+            // 
+            // btnKurangi
+            // 
+            btnKurangi.BackColor = Color.FromArgb(107, 114, 128);
+            btnKurangi.Cursor = Cursors.Hand;
+            btnKurangi.FlatStyle = FlatStyle.Flat;
+            btnKurangi.Font = new Font("Arial", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnKurangi.Location = new Point(338, 485);
+            btnKurangi.Name = "btnKurangi";
+            btnKurangi.Size = new Size(140, 38);
+            btnKurangi.TabIndex = 15;
+            btnKurangi.Text = "Kurangi";
+            btnKurangi.UseVisualStyleBackColor = false;
+            btnKurangi.Click += btnKurangi_Click;
+            // 
+            // Form1
+            // 
+            BackColor = Color.FromArgb(218, 41, 28);
+            ClientSize = new Size(632, 653);
+            Controls.Add(btnKurangi);
+            Controls.Add(btnRiwayat);
+            Controls.Add(btnSimpan);
+            Controls.Add(lblTotal);
+            Controls.Add(btnReset);
+            Controls.Add(btnHapus);
+            Controls.Add(lstPesanan);
+            Controls.Add(lblPesanan);
+            Controls.Add(btnTambah);
+            Controls.Add(cmbPilihMenu);
+            Controls.Add(lblPilihMenu);
+            Controls.Add(cmbKategori);
+            Controls.Add(lblKategori);
+            Controls.Add(txtNama);
+            Controls.Add(lblNama);
+            Controls.Add(lblJudul);
+            Font = new Font("Arial", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ForeColor = Color.White;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Menu Pesanan McDonald's";
+            Load += Form1_Load_1;
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+        private Label lblJudul;
+        private Label lblNama;
+        private TextBox txtNama;
+        private Label lblKategori;
+        private ComboBox cmbKategori;
+        private Label lblPilihMenu;
+        private ComboBox cmbPilihMenu;
+        private Button btnTambah;
+        private Label lblPesanan;
+        private ListBox lstPesanan;
+        private Button btnHapus;
+        private Button btnReset;
+        private Label lblTotal;
+        private Button btnSimpan;
+        private Button btnRiwayat;
+        private Button btnKurangi;
+    }
+}
---
+++ projek akhir/projekAkhir_RoichanAryshaputra/projekAkhir_RoichanAryshaputra/Form1.cs
@@ -0,0 +1,365 @@
+﻿namespace projekAkhir_RoichanAryshaputra
+{
+    public partial class Form1 : Form
+    {
+        //Membuat Variabel yang berisi data data dari class menu dan ItemPesanan
+        private List<Menu> menuItems;
+        private List<ItemPesanan> orderItems;
+        //Membuat Variabel yang berisi nama file
+        private string fileRiwayat = "riwayat_pesanan.txt";
+        public Form1()
+        {
+            //Inisialisasi komponen form
+            InitializeComponent();
+            //Inisialisasi list orderItems
+            orderItems = new List<ItemPesanan>();
+            //Memanggil method inisialisasi menuItems
+            initializemenuItems();
+
+        }
+
+        //Inisialisasi data menu
+        private void initializemenuItems()
+        {
+            //Menambahkan data data menu ke dalam list menuItems
+            menuItems = new List<Menu>
+            {
+                //kategori burger
+                new Menu { Id = 1, Nama = "Big Mac", Harga = 43500, Kategori = "Burger" },
+                new Menu { Id = 2, Nama = "Cheeseburger Deluxe", Harga = 34500, Kategori = "Burger" },
+                new Menu { Id = 3, Nama = "Triple Cheeseburger", Harga = 65500, Kategori = "Burger" },
+                new Menu { Id = 4, Nama = "Beef Burger Deluxe", Harga = 40000, Kategori = "Burger" },
+                new Menu { Id = 5, Nama = "Chicken Burger Deluxe", Harga = 30000, Kategori = "Burger" },
+                //kategori Ayam
+                new Menu { Id = 6, Nama = "PaMer 7 Ayam McD Gulai Krispy", Harga = 198500, Kategori = "Ayam" },
+                new Menu { Id = 7, Nama = "2 ALC Ayam Krispy McD Gulai", Harga = 50000, Kategori = "Ayam" },
+                new Menu { Id = 8, Nama = "PaNas 1 Ayam McD Gulai Krispy", Harga = 43000, Kategori = "Ayam" },
+                new Menu { Id = 9, Nama = "PaNas 2 Ayam McD Gulai Spicy Large", Harga = 66500, Kategori = "Ayam" },
+                new Menu { Id = 10, Nama = "2pcs Korean Soy Garlic Wings", Harga = 16500, Kategori = "Ayam" },
+                //kategori Makanan Pendamping 
+                new Menu { Id = 11, Nama = "French Fries (M)", Harga = 15000, Kategori = "Makanan Pendamping" },
+                new Menu { Id = 12, Nama = "French Fries(L)", Harga = 20000, Kategori = "Makanan Pendamping" },
+                new Menu { Id = 13, Nama = "Hash Brown", Harga = 15000, Kategori = "Makanan Pendamping" },
+                new Menu { Id = 14, Nama = "Apple Pie", Harga = 12000, Kategori = "Makanan Pendamping" },
+                 // kategori Minuman
+                new Menu { Id = 15, Nama = "Coca-Cola (M)", Harga = 12000, Kategori = "Minuman" },
+                new Menu { Id = 16, Nama = "Coca-Cola (L)", Harga = 15000, Kategori = "Minuman" },
+                new Menu { Id = 17, Nama = "Sprite (M)", Harga = 12000, Kategori = "Minuman" },
+                new Menu { Id = 18, Nama = "Sprite (L)", Harga = 15000, Kategori = "Minuman" },
+                new Menu { Id = 19, Nama = "Orange juice", Harga = 18000, Kategori = "Minuman" },
+                new Menu { Id = 20, Nama = "McCafe latte", Harga = 25000, Kategori = "Minuman" },
+                // kategori Makanan Penutup
+                new Menu { Id = 21, Nama = "McFlurry Oreo", Harga = 25000, Kategori = "Makanan Penutup" },
+                new Menu { Id = 22, Nama = "Sundae Chocolate", Harga = 15000, Kategori = "Makanan Penutup" },
+                new Menu { Id = 23, Nama = "Sunday Strawberry", Harga = 25000, Kategori = "Makanan Penutup" }
+            };
+            //Mengisi ComboBox Kategori dengan kategori unik dari menuItems
+            var kategori = menuItems.Select(m => m.Kategori).Distinct().OrderBy(k => k).ToList();
+            cmbKategori.Items.Add("=== Semua Kategori ===");
+            //Menambahkan setiap kategori ke dalam ComboBox
+            foreach (var Kategori in kategori)
+            {
+                cmbKategori.Items.Add(Kategori);
+            }
+            cmbKategori.SelectedIndex = 0;
+
+        }
+
+        //Membuat class menu
+        public class Menu
+        {
+            //Properti dari class menu
+            public int Id { get; set; }
+            public string Nama { get; set; }
+            public decimal Harga { get; set; }
+            public string Kategori { get; set; }
+            //Override method ToString untuk menampilkan nama dan harga menu
+            public override string ToString()
+            {
+                return $"{Nama} - Rp {Harga:N0}";
+            }
+
+        }
+        //Membuat class ItemPesanan
+        public class ItemPesanan
+        {
+            //Properti dari class ItemPesanan
+            public Menu Menu { get; set; }
+            public int Jumlah { get; set; } = 1;
+            //Constructor dari class ItemPesanan
+            public ItemPesanan(Menu item)
+            {
+                Menu = item;
+            }
+            //Override method ToString untuk menampilkan nama dan harga menu
+            public override string ToString()
+            {
+                return $"{Menu.Nama} x{Jumlah} - Rp {Menu.Harga * Jumlah:N0}";
+            }
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            // Cek apakah ada pesanan yang disimpan
+            if (orderItems.Count == 0)
+            {
+                MessageBox.Show("Tidak ada pesanan yang disimpan saat ini.", "Warning",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            // Mendapatkan nama pelanggan, jika kosong gunakan "Pelanggan"
+            string namapelanggan = string.IsNullOrWhiteSpace(txtNama.Text)
+                ? "Pelanggan" : txtNama.Text.Trim();
+            // Menyimpan riwayat pesanan ke file teks
+            try
+            {
+                // Membuka file riwayat dalam mode append
+                using (StreamWriter writer = new StreamWriter(fileRiwayat, true))
+                {
+                    // Menulis header riwayat pesanan
+                    writer.WriteLine("=".PadRight(70, '='));
+                    writer.WriteLine("             Riwayat Pesanan McDonald's");
+                    writer.WriteLine("=".PadRight(70, '='));
+                    writer.WriteLine($"Tanggal dan Jam: {DateTime.Now:dd/MM/yyyy HH:mm:ss}");
+                    writer.WriteLine($"Pelanggan: {namapelanggan}");
+                    writer.WriteLine("-".PadRight(70, '-'));
+                    writer.WriteLine("Detail Pesanan:");
+                    writer.WriteLine();
+
+                    int no = 1;
+
+                    // Menulis setiap item pesanan ke file
+                    foreach (var item in orderItems)
+                    {
+                        writer.WriteLine($"{no}. {item.Menu.Nama} x{item.Jumlah}  - Rp {item.Menu.Harga:N0}");
+                        no++;
+                    }
+                    decimal total = orderItems.Sum(o => o.Menu.Harga * o.Jumlah);
+                    writer.WriteLine();
+                    writer.WriteLine("-".PadRight(70, '-'));
+                    writer.WriteLine($"  {"TOTAL".PadRight(40)} Rp {total:N0}");
+                    writer.WriteLine("=".PadRight(70, '='));
+                    writer.WriteLine("         Terimakasih, Sampai berjumpa lagi!");
+                    writer.WriteLine("                  I'm lovin' it");
+                    writer.WriteLine("=".PadRight(70, '='));
+                    writer.WriteLine();
+                    writer.WriteLine();
+                }
+                MessageBox.Show($"pesanan berhasil di simpan !\n\nNama Pelanggan: {namapelanggan}\n" +
+                    $"Total Pembayaran: Rp {orderItems.Sum(o => o.Menu.Harga * o.Jumlah):N0}",
+                    "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Reset pesanan setelah disimpan
+                orderItems.Clear();
+                lstPesanan.Items.Clear();
+                txtNama.Clear();
+                cmbKategori.SelectedIndex = 0;
+                UpdateTotal();
+            }
+            // Menangani kesalahan saat menyimpan pesanan
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Gagal menyimpan pesanan. Error: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void cmbPilihMenu_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbKategori_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Memperbarui daftar menu berdasarkan kategori yang dipilih
+            cmbPilihMenu.Items.Clear();
+            cmbPilihMenu.Items.Add("=== Pilih Menu ===");
+
+            // Menampilkan semua menu jika kategori "Semua Kategori" dipilih
+            if (cmbKategori.SelectedIndex == 0)
+            {
+                // Tampilkan semua menu
+                foreach (var menu in menuItems)
+                {
+                    cmbPilihMenu.Items.Add(menu);
+                }
+            }
+
+            // Menampilkan menu berdasarkan kategori yang dipilih
+            else
+            {
+                string selectedCategory = cmbKategori.SelectedItem.ToString();
+                var filteredMenu = menuItems.Where(m => m.Kategori == selectedCategory).ToList();
+                foreach (var menu in filteredMenu)
+                {
+                    cmbPilihMenu.Items.Add(menu);
+                }
+            }
+            cmbPilihMenu.SelectedIndex = 0;
+        }
+
+        private void btnTambah_Click(object sender, EventArgs e)
+        {
+            // Menambahkan item pesanan ke daftar pesanan
+            if (cmbPilihMenu.SelectedIndex > 0)
+            {
+                Menu selectedMenu = (Menu)cmbPilihMenu.SelectedItem;
+                // Cek apakah item pesanan sudah ada dalam daftar
+                var existingItem = orderItems.FirstOrDefault(o => o.Menu.Id == selectedMenu.Id);
+                // Jika sudah ada, tambahkan jumlahnya
+                if (existingItem != null)
+                {
+                    // Menambahkan jumlah item pesanan
+                    existingItem.Jumlah++;
+                    // Memperbarui tampilan item pesanan di ListBox
+                    int index = lstPesanan.Items.IndexOf(existingItem);
+                    lstPesanan.Items[index] = existingItem;
+                }
+                else
+                {
+                    ItemPesanan itemPesanan = new ItemPesanan(selectedMenu);
+                    orderItems.Add(itemPesanan);
+                    lstPesanan.Items.Add(itemPesanan);
+                }
+
+                UpdateTotal();
+            }
+            // Jika tidak ada menu yang dipilih, tampilkan peringatan
+            else
+            {
+                MessageBox.Show("Silakan pilih menu terlebih dahulu.", "Peringatan",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            // Menghapus item pesanan yang dipilih dari daftar pesanan
+            if (lstPesanan.SelectedIndex >= 0)
+            {
+                ItemPesanan selectedItem = (ItemPesanan)lstPesanan.SelectedItem;
+                orderItems.Remove(selectedItem);
+                lstPesanan.Items.Remove(selectedItem);
+                UpdateTotal();
+            }
+            // Jika tidak ada item yang dipilih, tampilkan peringatan
+            else
+            {
+                MessageBox.Show("Silakan pilih item pesanan yang akan dihapus.", "Peringatan",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        // Memperbarui total harga pesanan
+        private void UpdateTotal()
+        {
+            // Menghitung total harga dari semua item pesanan
+            decimal total = orderItems.Sum(o => o.Menu.Harga * o.Jumlah);
+            lblTotal.Text = $"Total: Rp {total:N0}";
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // Mereset daftar pesanan setelah konfirmasi dari pengguna
+            if (orderItems.Count > 0)
+            {
+                // Menampilkan dialog konfirmasi
+                DialogResult result = MessageBox.Show("Apakah Anda yakin ingin mereset pesanan?",
+                    "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                // Jika pengguna memilih "Yes", reset daftar pesanan
+                if (result == DialogResult.Yes)
+                {
+                    orderItems.Clear();
+                    lstPesanan.Items.Clear();
+                    UpdateTotal();
+                }
+            }
+        }
+
+        // Membuka file riwayat pesanan dengan Notepad
+        private void btnRiwayat_Click(object sender, EventArgs e)
+        {
+            // Memanggil method untuk membuka riwayat dengan Notepad
+            BukaRiwayat();
+        }
+
+        // Method untuk membuka file riwayat pesanan dengan Notepad
+        private void BukaRiwayat()
+        {
+            // Cek apakah file riwayat ada
+            try
+            {
+                // Jika file tidak ada, tampilkan pesan informasi
+                if (!File.Exists(fileRiwayat))
+                {
+                    MessageBox.Show("Belum ada riwayat pesanan.\n\nSilakan lakukan checkout terlebih dahulu.",
+                        "File Tidak Ditemukan",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                    return;
+                }
+                // Membuka file riwayat dengan Notepad
+                System.Diagnostics.Process.Start("notepad.exe", fileRiwayat);
+            }
+            // Menangani kesalahan saat membuka file riwayat
+            catch (Exception ex)
+            {
+                // Menampilkan pesan error jika gagal membuka file
+                MessageBox.Show($"Gagal membuka file riwayat!\n\nError: {ex.Message}",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKurangi_Click(object sender, EventArgs e)
+        {
+            // Mengurangi jumlah item pesanan yang dipilih
+            if (lstPesanan.SelectedIndex >= 0)
+            {
+                int selectedIndex = lstPesanan.SelectedIndex;
+                ItemPesanan selectedItem = (ItemPesanan)lstPesanan.SelectedItem;
+                // Cek apakah jumlah item lebih dari 1
+                if (selectedItem.Jumlah > 1)
+                {
+                    // Kurangi jumlah item pesanan
+                    selectedItem.Jumlah--;
+
+                    // Update tampilan ListBox
+                    lstPesanan.Items.Clear();
+                    // tambah semua item pesanan kembali ke ListBox
+                    foreach (var item in orderItems)
+                    {
+                        lstPesanan.Items.Add(item);
+                    }
+
+                    lstPesanan.SelectedIndex = selectedIndex;
+                }
+                // Jika jumlah item adalah 1, hapus item dari daftar
+                else
+                {
+                    orderItems.Remove(selectedItem);
+                    lstPesanan.Items.Remove(selectedItem);
+                }
+
+                UpdateTotal();
+            }
+            else
+            {
+                MessageBox.Show("Silakan pilih item pesanan yang akan dikurangi.", "Peringatan",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void lstPesanan_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
